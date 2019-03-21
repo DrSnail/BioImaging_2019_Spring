18c43ece3a2436d881cdfa20c696b416d4f31dbe
DrSnail <guardfeasul@gmail.com>
2019-03-21 20:25:41 +0700

    Добавил методы в базовый класс. Добавил исключение

--- Classes/Exceptions.py
+++ Classes/Exceptions.py
@@ -1,3 +1,7 @@
 class ImageError(Exception):
     def __str__(self):
         return "Error"
+
+class ImageIsNotPresented(ImageError):
+    def __str__(self):
+        return "Image have to be specifed before using"
--- Classes/Image.py
+++ Classes/Image.py
@@ -1,9 +1,25 @@
 import cv2
+from Classes.Exceptions import *
 
 class Image:
     def __init__(self, address: str):
         self._address = address
         pass
+
+    def read(self):
+        self.image = cv2.imread(self._address)
+
+    def save(self):
+        try:
+            cv2.imwrite(self._address, self.image)
+        except Exception as e:
+            raise ImageIsNotPresented()
+
+    def get_address(self):
+        return self._address
+
+    def set_address(self, address):
+        self._address = address
 
 class Grey_Image(Image):
     def __init__(self, address: str):
